e6a2582c8cac1d4aa17db06fba3a54daad2a3c55
Maxsuel Maccari <maxmaccari@hotmail.com>
2019-04-28 10:48:07 -0400

    Add watch and default tasks

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,8 @@ const buildStyles = () => {
       browsers: ['last 2 versions'],
       cascade: false
     }))
-    .pipe(gulp.dest('css/'));
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.stream());
 };
 
 const server = () => {
@@ -19,9 +20,16 @@ const server = () => {
     server: {
       baseDir: './'
     }
-  })
+  });
 };
+
+const watch = () => {
+  gulp.watch('scss/*.scss', buildStyles);
+  gulp.watch('./**/*.html').on('change', browserSync.reload);
+}
 
 // Tasks
 gulp.task('styles', buildStyles);
 gulp.task('server', server);
+gulp.task('watch', watch);
+gulp.task('default', gulp.parallel('watch', 'server'));
